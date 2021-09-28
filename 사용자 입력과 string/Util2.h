7fc934f61343f341c268d012909eebe348e92e65
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-09-28 22:50:04 +0900

    나의 첫 프로그램

---
+++ 사용자 입력과 string/Util2.h
@@ -0,0 +1,7 @@
+#pragma once
+//선언 해드는 선언만 한다
+int Add(int First, int Second);
+int Sub(int First, int Second);
+int Mul(int First, int Second);
+int Div(int First, int Second);
+int Mod(int First, int Second);
