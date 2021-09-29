73be65318573f4386330d8fca4d532e0e0c751de
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-09-30 00:19:45 +0900

    Study

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -2,15 +2,63 @@
 
 using namespace std;
 
+void EvenNumber();
+void OddNumber();
+void Star();
+
+
+
+
 int main()
 {
-	
 
-	int AP = 512;
+	int b = 0;
 
+	for (int a = 0; a <= 100; a++)
+	{
+			b = b + a;
+	}
 
+	cout << b << endl;
 
-	cout << AP << endl;
+	EvenNumber();
+	OddNumber();
+	Star();
 
 	return 0;
+}
+
+void EvenNumber()
+{
+	int a = 0;
+
+	for (int b = 0; b <= 100; b = b + 2)
+	{
+		a = a + b;
+	}
+
+	cout << a << endl;
+
+}
+
+void OddNumber()
+{
+	int a = 0;
+	for (int b = 1; b < 100; b = b + 2)
+	{
+		a = a + b;
+	}
+
+	cout << a << endl;
+}
+
+void Star()
+{
+	for (int a = 0; a < 5; a++)
+	{
+		for (int b = 0; b < 5; b++)
+		{
+			cout << '*';
+		}
+	}
 }
