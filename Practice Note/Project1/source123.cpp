f726344ab4b2f17520aa5aa8172f2949143caea5
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-09-30 23:43:09 +0900

    고양이

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -8,34 +8,10 @@ bool Program = true;
 
 int main()
 {
-	int a;
-	int b;
-
-
-	while (Program)
-	{
-
-
-		cout << "첫번째 덧셈할 숫자키 하나를 눌러주세요!" << endl;
-		
-		cin >> a;
-
-		if (a == 10)
-		{
-
-			Program = false;
-
-			continue; // 아래남은 코드들을 무시하고 위로올라간다.
-		}
-
-		cout << "두번째 덧셈할 숫자키 하나를 눌러주세요!" << endl;
-
-		cin >> b;
-
-		cout << a + b << endl;
+	cout << " ＼      /＼   " << endl;
+	cout << "   )    (  ')  " << endl;
+	cout << "  (   /   )    " << endl;
+	cout << "   ＼(__)|     " << endl;
 	
-	}
-	cout << "10을 눌러 종료됩니다." << endl;
-	
-	return 0;
+
 }
