39fd4d5bd1e1d11ad62889e918dac715a6ddf8af
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-09-30 23:28:22 +0900

    Homework

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -14,9 +14,19 @@ int main()
 
 	while (Program)
 	{
+
+
 		cout << "첫번째 덧셈할 숫자키 하나를 눌러주세요!" << endl;
 		
 		cin >> a;
+
+		if (a == 10)
+		{
+
+			Program = false;
+
+			continue; // 아래남은 코드들을 무시하고 위로올라간다.
+		}
 
 		cout << "두번째 덧셈할 숫자키 하나를 눌러주세요!" << endl;
 
@@ -25,6 +35,7 @@ int main()
 		cout << a + b << endl;
 	
 	}
-
-
+	cout << "10을 눌러 종료됩니다." << endl;
+	
+	return 0;
 }
