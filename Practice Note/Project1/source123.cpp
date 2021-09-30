5550f4eaf6ce87e0ddcb247176875d415aca2ada
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-10-01 00:47:47 +0900

    하나 성공

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -2,29 +2,42 @@
 
 using namespace std;
 
-void Infut();
+//숫자외에 입력되면 끝내는 프로그램 까지 만들려다 실패 Goto 활용을 못하겠다 ㅠ
 
 bool GameState = true;
 
 int main()
 {
-	int a;
-	int b;
+	int a = 0;
+	int b = 0;
+
+	string c;
+	
 	while (GameState)
-	{	
+	{
 		cin >> a;
-		if (!a)
+		
+		cin >> b;
+
+		if (a < b)
+		{
+			cout << b << " 가 " << a << " 보다 큽니다!" << endl;
+		}
+		else if (a > b)
+		{
+			cout << a << " 가 " << b << " 보다 큽니다!" << endl;
+		}
+		else if (a == b)
+		{
+			cout << a << " 와 " << b << " 가 같습니다. !" << endl;
+		}
+		/*else if ( a!=a || b!= b)
 		{
 			GameState = false;
-		}
-		cin >> b;
-		
-		
-
-		
-		cout << a + b << endl;
+		}*/
 	}
 
-	
 
+	
+	return 0 ;
 }
