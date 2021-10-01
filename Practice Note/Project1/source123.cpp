752bfc82f109128a980f9f89381514bd26bbed18
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-10-02 02:17:08 +0900

    연습

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
@@ -8,36 +9,49 @@ bool GameState = true;
 
 int main()
 {
-	int a = 0;
-	int b = 0;
-
-	string c;
+	int a;
+	int b;
+	char c;
 	
-	while (GameState)
+	/*int a[b];
+	int c[d];*/
+	
+	bool Program = true;
+	//a 나 b 에 0~10 이외에 숫자를 입력하게되면 false 되게하는 식을 구해보자
+
+
+	
+
+
+	while (Program)
 	{
-		cin >> a;
-		
-		cin >> b;
-
-		if (a < b)
-		{
-			cout << b << " 가 " << a << " 보다 큽니다!" << endl;
-		}
-		else if (a > b)
-		{
-			cout << a << " 가 " << b << " 보다 큽니다!" << endl;
-		}
-		else if (a == b)
-		{
-			cout << a << " 와 " << b << " 가 같습니다. !" << endl;
-		}
-		/*else if ( a!=a || b!= b)
-		{
-			GameState = false;
-		}*/
-	}
-
-
 	
+		cin >> a;
+			
+		cin >> b;
+		
+		
+		
+		if (a == _getch())
+		{
+			Program = false;
+		}
+		else if (a <= 10 && b <= 10)
+		{
+		cout << a << " 와 " << b << " 를 더한 값" << endl;
+		cout << a + b << " 입니다 " << endl;
+			
+			
+
+		}
+
+		else
+		{
+			cout << "0부터 ~ 10 까지 숫자를 입력해주세요" << endl;
+			Program = false;
+		}
+	
+	
+	}
 	return 0 ;
 }
