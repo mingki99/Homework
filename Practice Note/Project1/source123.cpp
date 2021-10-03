a9fdcb1478012f4f907db62deba44e53e34074e7
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-10-03 22:44:45 +0900

    I'm a bast

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -2,19 +2,37 @@
 
 using namespace std;
 
+/*한달 간 고정으로 지출되는 고정 비용을 A
+		물건 하나를 만들 때 들어가는 가변 비용을 B
+		물건의 가격을 C
+		라고 하였을때,
+.
+		A, B, C를 입력받으면 손익분기점을 구하는 프로그램을 구하시오. (손익분기점이 존재하지 않으면 - 1을 출력한다)*/
 int main()
 {
 
-	// 배열 정수 넣어주기
-	int a[10];
+	int a;
+	int b;
+	int c;
 
-	int i;
-	for (i = 1; i <= 10; i++)
+	int d;
+
+	cin >> a;
+	cin >> b;
+	cin >> c;
+
+
+	if (b < c)
 	{
-		a[i] = i;
-		cout << a[i] << endl;
+		for (d = 0; d * c < a + b * d; d++)
+		{
+		}
+		cout << "손익분기점을 넘기는 판매 갯수는 " << d + 1 << "입니다" << endl;
 	}
-
+	else(c < b);
+	{
+		cout << "인건비도 안나오겠네;" << " -1" << endl;
+	}
 	
 	
 }
