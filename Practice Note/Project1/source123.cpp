e54728abb768293cc0b36786662fcb02b6e962a0
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-10-15 00:34:54 +0900

    클래스연습

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -1,66 +1,20 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-void TastA(int&, int&);
+// HP MP MOVE ATTEC 가 있는 플레이어.
+// 돼지 슬라임 고블린 
+// 플레이어는 Gold 가 
+//몬스터에게는 running away 가 있음
 
-void TastB(int*, int*);
 
-void TastC(int, int);
+
+
+
+
 
 int main()
 {
-	int Homwork1 = 100;
-	int Homwork2 = 200;
+	return 0;
 
-	cout << "첫 정수 상태" << endl;
-	cout << Homwork1 << '\n' << Homwork2 << endl;
-
-	TastA(Homwork1, Homwork2);
-	cout << "참조 변수" << endl;
-	cout << Homwork1 << '\n' << Homwork2 << endl;
-
-	TastB(&Homwork1, &Homwork2);
-	cout << "포인터 이용" << endl;
-	cout << Homwork1 << '\n' << Homwork2 << endl;
-
-	TastC(Homwork1, Homwork2);
-	cout << "그냥 함수" << endl;
-	cout << Homwork1 << '\n' << Homwork2 << endl;
-
-
-
-}
-
-
-void TastA(int& a, int& b)
-{
-	int tamp;
-
-	tamp = a;
-	a = b;
-	b = tamp;
-
-}
-
-void TastB(int* a, int* b)
-{
-	int tamp;
-
-	tamp = *a;
-	*a = *b;
-	*b = tamp;
-
-}
-
-void TastC(int a, int b)
-{
-	int tamp;
-
-	tamp = a;
-	a = b;
-	b = tamp;
-
-	cout << "테스트 안에서는?" << endl;
-	cout << a << '\n' << b << endl;
 }
