51426efb71e12d66a853d41c754b235d638adac6
jung min ki <dkwnajsquf@naver.com>
2021-10-17 15:55:06 +0900

    어떻게 불러올까

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -1,80 +1,29 @@
 #include<iostream>
+#include "Engin.h"
+#include "Player.h"
+#include "Slime.h"
+#include "MonsterPlayBattle.h"
+
 
 using namespace std;
 
-<<<<<<< HEAD
+
 // HP MP MOVE ATTEC 가 있는 플레이어.
 // 돼지 슬라임 고블린 
 // 플레이어는 Gold 가 
 //몬스터에게는 running away 가 있음
 
 
-
-
-=======
-// 0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다.
-// 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 
-// 각 자리의 숫자를 더한다.그 다음, 
-// 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다.다음 예를 보자.
-//
-// 26부터 시작한다. 2 + 6 = 8이다.새로운 수는 68이다. 6 + 8 = 14이다.새로운 수는 84이다. 8 + 4 = 12이다.새로운 수는 42이다.
-//  4 + 2 = 6이다.새로운 수는 26이다.
-//
-// 위의 예는 4번만에 원래 수로 돌아올 수 있다.따라서 26의 사이클의 길이는 4이다.
-//
-// N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
-
-// 첫째 줄에 N이 주어진다.N은 0보다 크거나 같고, 99보다 작거나 같은 정수이다.
-
-
 int main()
 {
-	int a = 0;
-	cin >> a;
+	MonsterPlayBattle monsterPlayBattle;
 
-	int b = a % 10;
-	int c = 0;
-	a = a / 10;
-	
+	monsterPlayBattle.MonsterPlayBattle();
 
 	
-	int cicle = 0;
-	
-	bool start = true;
-
-	while (start)
-	{
-		c = a + b;
-
-		c = c % 10;
->>>>>>> ceaf36022b3f4144757c0c3cca371db2d32fe2bb
-
-		a = b + c;
-
-		a = a % 10;
-
-<<<<<<< HEAD
-int main()
-{
-=======
-		b = c + a;
-
-		b = b % 10;
-
-		cicle++;
-
-		if ((a, b) == (b , c))
-		{
-			cout << cicle + 3 << endl;
-			start = false;
-		}
-		
-
-	}
-
 	
 
->>>>>>> ceaf36022b3f4144757c0c3cca371db2d32fe2bb
+
 	return 0;
 
 }
