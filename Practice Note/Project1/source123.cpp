7223c86747114576276b5503ddc7d934c23aa453
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-11-11 22:04:22 +0900

    공부

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -3,49 +3,54 @@
 #include<time.h>
 
 using namespace std;
+//코드수정 유지보수하기 편하다.
+#define NAME_SIZE 32
 
 /*
-0 : 벽
-1 : 길
-2 : 시작점
-3 : 도착점
-*/
+열거체 : 숫자의 이름 을 부여해줌
 
-void SetMaze(char Maze[20][20])
+구조체 : 관련있는 변수들을 모아서 하나의 새로운 타입을 만들어주는 기능이다.
+사용자 정의 변수 타입이다.
+
+형태 : struct 구조체명 {};  의 형태로 구성된다.
+
+배얄과 구조체의 공통점 
+1. 데이터 집합이다
+2. 연속된 메모리 블럭에 할당된다. 구조체 멤버들은 연속된 메모리 
+블럭으로 잡히게 된다.
+
+*/
+struct _tagStudent
 {
-	strcpy_s(Maze[0],  "2110000000000000000");
-	strcpy_s(Maze[1],  "0011111111111100000");
-	strcpy_s(Maze[2],  "0000000000000000000");
-	strcpy_s(Maze[3],  "0000000000000000000");
-	strcpy_s(Maze[4],  "0000000000000000000");
-	strcpy_s(Maze[5],  "0000000000000000000");
-	strcpy_s(Maze[6],  "0000000000000000000");
-	strcpy_s(Maze[7],  "0000000000000000000");
-	strcpy_s(Maze[8],  "0000000000000000000");
-	strcpy_s(Maze[9],  "0000000000000000000");
-	strcpy_s(Maze[10], "0000000000000000000");
-	strcpy_s(Maze[11], "0000000000000000000");
-	strcpy_s(Maze[12], "0000000000000000000");
-	strcpy_s(Maze[13], "0000000000000000000");
-	strcpy_s(Maze[14], "0000000000000000000");
-	strcpy_s(Maze[15], "0000000000000000000");
-	strcpy_s(Maze[16], "0000000000000000000");
-	strcpy_s(Maze[17], "0000000000000000000");
-	strcpy_s(Maze[18], "0000000000000000000");
-	strcpy_s(Maze[19], "0000000000000000000");
-	Maze = 
-	{
-		{"2110000000000000000"}
-	}
-}
+	char	strName[NAME_SIZE];
+	int		iNumber;
+	int		iKor;
+	int		iEng;
+	int		iMath;
+	int		iTotal;
+	float	fAvg
+};
 
 int main()
 {
-	// 20*20 미로를 만들어준다.
+	_tagStudent tStudent = {};
+	_tagStudent tStudentArr[100] = {};
 
-	char strMaze[20][20];
+	//구조체 멤버에 접근할때는 . 을 이용해서 접근하게 된다.
 
+	tStudent.iKor = 100;
+
+	//tStudent.strName
+	// 문자열을 배열에 넣어줄때에는 단순 대입으로는 불가능하다.
+	// strcpy_s 라는 함수를 이용 , 문자열을 복사해야한다.
+
+	strcpy_s(tStudent.strName, "가나다라 ASDF");
+
+	cout << "이름 : " << tStudent.
 
 	return 0;
 }
+
+
+
 
