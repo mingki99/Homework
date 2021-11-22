5bb8450f90533335122408a02e69eaf04a6d1bd9
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-11-22 22:02:09 +0900

    비트연산자

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -3,50 +3,44 @@
 #include<time.h>
 
 using namespace std;
-//코드수정 유지보수하기 편하다.
-#define NAME_SIZE 32
-
-/*
-열거체 : 숫자의 이름 을 부여해줌
-
-구조체 : 관련있는 변수들을 모아서 하나의 새로운 타입을 만들어주는 기능이다.
-사용자 정의 변수 타입이다.
-
-형태 : struct 구조체명 {};  의 형태로 구성된다.
-
-배얄과 구조체의 공통점 
-1. 데이터 집합이다
-2. 연속된 메모리 블럭에 할당된다. 구조체 멤버들은 연속된 메모리 
-블럭으로 잡히게 된다.
-
-*/
-struct _tagStudent
-{
-	char	strName[NAME_SIZE];
-	int		iNumber;
-	int		iKor;
-	int		iEng;
-	int		iMath;
-	int		iTotal;
-	float	fAvg
-};
-
 int main()
 {
-	_tagStudent tStudent = {};
-	_tagStudent tStudentArr[100] = {};
+	// 상수 : 변하지 않는 수. 값을 한번 지정해 놓으면 바꿀 수 없다.
+	// 상수는 선언과 동시에 값을 지정해두어야 한다.
 
-	//구조체 멤버에 접근할때는 . 을 이용해서 접근하게 된다.
+	const int	iAttack = 0x00000001;	//    1
+	const int	iArmor = 0x00000002;	//   10
+	const int	iHP = 0x00000004;		//  100
+	const int	iMP = 0x00000008;		// 1000
+	const int	iCritical = 0x00000010; //10000
 
-	tStudent.iKor = 100;
+	int iBuf = iAttack | iHP | iCritical;
 
-	//tStudent.strName
-	// 문자열을 배열에 넣어줄때에는 단순 대입으로는 불가능하다.
-	// strcpy_s 라는 함수를 이용 , 문자열을 복사해야한다.
+	iBuf ^= iHP;
 
-	strcpy_s(tStudent.strName, "가나다라 ASDF");
+	iBuf ^= iHP;
+	
+	
+	
+	cout << "Attack : " << (iBuf & iAttack) << endl;
 
-	cout << "이름 : " << tStudent.
+	cout << "Armor : " << (iBuf & iArmor) << endl;
+	cout << "HP : " << (iBuf & iHP) << endl;
+	cout << "MP : " << (iBuf & iMP) << endl;
+	cout << "Armor : " << (iBuf & iCritical) << endl;
+
+
+	int iHigh = 187;
+	int iLow = 13560;
+
+	int iNumber = iHigh;
+
+	iNumber <<= 16;
+
+	iNumber |= iLow;
+
+	cout << "High : " << (iNumber >> 16) << endl;
+	cout << "Low : " << (iNumber & 0x0000ffff) << endl;
 
 	return 0;
 }
