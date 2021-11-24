0b78119a784b7addff5671dd50ef5e18b6345938
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-11-24 22:40:15 +0900

    If 문 을 활용한 강화!

--- Practice Note/Project1/source123.cpp
+++ Practice Note/Project1/source123.cpp
@@ -5,42 +5,63 @@
 using namespace std;
 int main()
 {
-	// 상수 : 변하지 않는 수. 값을 한번 지정해 놓으면 바꿀 수 없다.
-	// 상수는 선언과 동시에 값을 지정해두어야 한다.
+	srand((unsigned int)time(0));
 
-	const int	iAttack = 0x00000001;	//    1
-	const int	iArmor = 0x00000002;	//   10
-	const int	iHP = 0x00000004;		//  100
-	const int	iMP = 0x00000008;		// 1000
-	const int	iCritical = 0x00000010; //10000
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << (rand() % 101 + 100) << endl;
+	cout << (rand() % 10000 / 100.f) << endl;
 
-	int iBuf = iAttack | iHP | iCritical;
-
-	iBuf ^= iHP;
-
-	iBuf ^= iHP;
+	int iUpgrade = 0;
+	cout << "Upgrade 기본 수치를 입력하세요 : ";
 	
+
+	//강화 확률 구한다.
 	
-	
-	cout << "Attack : " << (iBuf & iAttack) << endl;
 
-	cout << "Armor : " << (iBuf & iArmor) << endl;
-	cout << "HP : " << (iBuf & iHP) << endl;
-	cout << "MP : " << (iBuf & iMP) << endl;
-	cout << "Armor : " << (iBuf & iCritical) << endl;
+	while (true)
+	{
+		cin >> iUpgrade;
+		
+		float	fPercent = rand() % 10000 / 100.f;
 
+	//강화 확률 : 업그레이드가 0~3 : 100% 성공 , 4~6 : 40% 7~9 : 10%
+	// 10~13 : 1% , 14 ~ 15 : 0.1 %
+	cout << "Upfrade : " << iUpgrade << endl;
+	cout << "Percent : " << fPercent << endl;
 
-	int iHigh = 187;
-	int iLow = 13560;
-
-	int iNumber = iHigh;
-
-	iNumber <<= 16;
-
-	iNumber |= iLow;
-
-	cout << "High : " << (iNumber >> 16) << endl;
-	cout << "Low : " << (iNumber & 0x0000ffff) << endl;
+	if (iUpgrade <= 3)
+		cout << "강화 성공" << endl;
+	else if (4 <= iUpgrade && iUpgrade <=6)
+	{
+		if (fPercent < 40.f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	else if (7 <= iUpgrade && iUpgrade <= 9)
+	{
+		if (fPercent < 10.f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	else if (10 <= iUpgrade && iUpgrade <= 13)
+	{
+		if (fPercent < 1.f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	else if (14 <= iUpgrade && iUpgrade <= 15)
+	{
+		if (fPercent < 0.1f)
+			cout << "강화 성공" << endl;
+		else
+			cout << "강화 실패" << endl;
+	}
+	}
 
 	return 0;
 }
