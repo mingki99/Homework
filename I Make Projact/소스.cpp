0dbcf3bac26121e1dd3691371b788c6f6c2a9f87
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-11-28 16:38:09 +0900

    Lotto

--- I Make Projact/소스.cpp
+++ I Make Projact/소스.cpp
@@ -1,106 +1,37 @@
 #include <iostream>
-#include <windows.h>
-
+#include <time.h>
 
 using namespace std;
 
-int Infut();
-void Porcess(int key);
-void Rander();
-
-void GoldPorcess(int key);
-void GoldRander();
-
-bool bGameState = true;
-
-int Gold;
-
 int main()
 { 
-	while (bGameState)
+	//Lotti Program
+	int iLotto[45] = {};
+
+	// 1 ~ 45 까지의 숫자를 차례대로 넣어준다.
+	for (int i = 0; i < 45; ++i)
 	{
-		int key = Infut();
-		 Porcess(key);
-		 Rander();
+		iLotto[i] = i + 1;
+	}
+
+	int iTemp, idx1, idx2;
+
+	for (int i = 0; i < 100; ++i)
+	{
+		idx1 = rand() % 45;
+		idx2 = rand() % 45;
+
+		iTemp = iLotto[idx1];
+		iLotto[idx1] = iLotto[idx2];
+		iLotto[idx2] = iTemp;
+
+	}
+
+	for (int i = 0; i < 6; ++i)
+	{
+		cout << iLotto[i] << endl;
 	}
 
 	return 0;
 }
 
-int Infut() //사용자의 출력값을 받자
-{
-	int a;
-
-	cin >> a;
-
-	return a; //return  을 할려면 void 형식이면 xx
-
-}
-
-
-
-
-
-void Porcess(int key)
-{
-	if (key == 0)
-	{
-		bGameState = false;
-	}
-	GoldPorcess(key);
-}
-
-void Rander() 
-{
-	system("cls");
-	cout << "돈벌시간" << endl;
-
-	GoldRander();
-}
-void GoldPorcess(int key)
-{
-	if (key == 1)
-	{
-		Gold++;
-	}
-	else if (key == 2)
-	{
-		Gold = Gold + 2;
-	}
-	else if (key == 3)
-	{
-		Gold = Gold + 3;
-	}
-	else if (key == 4)
-	{
-		Gold = Gold + 4;
-	}
-	else if (key == 5)
-	{
-		Gold = Gold + 5;
-	}
-	else if (key == 6)
-	{
-		Gold = Gold + 6;
-	}
-	else if (key == 7)
-	{
-		Gold = Gold + 7;
-	}
-	else if (key == 8)
-	{
-		Gold = Gold + 8;
-	}
-	else if (key == 9)
-	{
-		Gold = Gold + 9;
-	}
-	else 
-	{}
-	
-}
-
-void GoldRander()
-{
-	cout << "획득골드 :" << Gold << endl;
-}
