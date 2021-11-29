1254079637c91b0d3c4772225f56c094899307c7
mingki99 <90670024+mingki99@users.noreply.github.com>
2021-11-29 22:39:50 +0900

    야구게임

--- I Make Projact/소스.cpp
+++ I Make Projact/소스.cpp
@@ -5,32 +5,91 @@ using namespace std;
 
 int main()
 { 
-	//Lotti Program
-	int iLotto[45] = {};
+	
+	int iNumber[9] = {};
 
-	// 1 ~ 45 까지의 숫자를 차례대로 넣어준다.
-	for (int i = 0; i < 45; ++i)
+	for (int i = 0; i < 9; ++i)
 	{
-		iLotto[i] = i + 1;
+		iNumber[i] = i + 1;
 	}
 
 	int iTemp, idx1, idx2;
 
 	for (int i = 0; i < 100; ++i)
 	{
-		idx1 = rand() % 45;
-		idx2 = rand() % 45;
+		idx1 = rand() % 9;
+		idx2 = rand() % 9;
 
-		iTemp = iLotto[idx1];
-		iLotto[idx1] = iLotto[idx2];
-		iLotto[idx2] = iTemp;
+		iTemp = iNumber[idx1];
+		iNumber[idx1] = iNumber[idx2];
+		iNumber[idx2] = iTemp;
 
 	}
 
-	for (int i = 0; i < 6; ++i)
+	cout << iNumber[0] << "\t" << iNumber[1] << "\t" << iNumber[2] << endl;
+
+
+	int iStrike = 0, iBall = 0;
+	int iInput[3];
+
+	while (true)
 	{
-		cout << iLotto[i] << endl;
+		cout << "1~ 9 사이의 숫자중 숫자 3개를 입력하세요 (0 : 종료) : ";
+		cin >> iInput[0] >> iInput[1] >> iInput[2];
+
+		if (iInput[0] == 0 || iInput[1] == 0 || iInput[2] == 0)
+			break;
+		else if (iInput[0] < 0 || iInput[1] < 0 || iInput[2] < 0 || iInput[0] > 9 || iInput[1] > 9 || iInput[2] > 9)
+		{
+			cout << "잘못된 숫자를 입력하였습니다." << endl;
+			continue;
+		}
+
+		else if (iInput[0] == iInput[1] || iInput[0] == iInput[2] || iInput[1] == iInput[2])
+		{
+			cout << "중복된 숫자를 입력하였습니다." << endl;
+			continue;
+		}
+
+		iStrike = iBall = 0;
+
+		for (int i = 0; i < 3; ++i)
+		{
+			for (int j = 0; j < 3; ++j)
+			{
+				if (iNumber[i] == iInput[j])
+				{
+					if (i == j)
+						++iStrike;
+					else
+						++iBall;
+					break;
+			
+
+
+				}
+			}
+		}
+
+		if (iStrike == 3)
+		{
+			cout << "홈런~" << endl;
+			break;
+		}
+		else if (iStrike == 0 && iBall == 0)
+		{
+			cout << "아웃입니다." << endl;
+		}
+		else
+		{
+			cout << iStrike << "strike" << iBall << "ball" << endl;
+		}
+
+
+
+
 	}
+
 
 	return 0;
 }
