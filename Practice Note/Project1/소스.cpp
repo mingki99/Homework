1f4d6c941282c15dd46575f13dc6ab4a88a34b69
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-02-02 22:59:44 +0900

    byte 의 양수와 음수

--- Practice Note/Project1/소스.cpp
+++ Practice Note/Project1/소스.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int iCount ;
-
-int iNumber;
-
-
-int main(int argc, const char* argv[])
+int main()
 {
+	// 자료형 (크기단위, Byte)
+	// 정수형 : cahr(1), short(2), int(4), long(4), long ling(8)
+	// 실수형 : float(4), double(8)
 
-	cin >> iCount;
+	int i;
 
-	int iarray[1000001];
-	
-	for (int i = 0; i <= iCount; i++)
-	{
-		cin >> iarray[i];
-		
-	}
+	//양수만 표현 unsigned
+	// 1 바이트로 양수, 음수 둘다 표현.
+	// 256 가지 -> 0~255
+	unsigned char c = 0;
+	c = 0;
+	c = -1; // c= 255
 
-	sort(iarray + 0, iarray + iCount);
+	// 1 바이트로 양수, 음수 표현.
+	// 256가지 -128 ~ 0 ~ 127
+	char c1 = 0;
+	c1 = 255; // c = -1
 
-	cout << iarray[0] << " " << iarray[iCount-1] << endl;
-
-	return 0;
+	cout << c << endl;
 }
 
