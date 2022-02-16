442198a339bd64b819198f38828459999072ccd6
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-02-16 23:55:43 +0900

    함수 2

--- Practice Note/Project1/source.cpp
+++ Practice Note/Project1/source.cpp
@@ -17,7 +17,7 @@ int Factorial(int _iNumb)
 		iValue *= (j + 2);
 	}
 
-	Factorial(10);
+	
 
 	return iValue;
 }
@@ -27,13 +27,44 @@ int Factorial(int _iNumb)
 
 // 성능이 떨어진다.
 
+int Factorial_Re(int _iNum)
+{
+	if (1 == _iNum)
+	{
+		return 1;
+	}
+
+	return _iNum * Factorial_Re(_iNum - 1);
+}
+
+
+// 피보나치 수열
+// 1 1 2 3 5 8 13 21 34 55...
+
+int Fibonacci(int _iNum)
+{
+	// 1,2 번째는 값이 1과 1이기 때문에 
+	if (1 == _iNum || 2 == _iNum)
+	{
+		return 1;
+	}
+
+	for (int i = 0; i < _iNum - 2; ++i)
+	{
+
+	}
+}
+
+
 
 int main()
 {
 	int iValue = Factorial(4);
 	iValue = Factorial(10);
 
-
+	iValue = Factorial_Re(10);
+	
+	iValue = Fibonacci(10);
 
 	return 0;
 }
