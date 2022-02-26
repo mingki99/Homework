bf289c9325819f8ee21b81e7376095fe8138bfdd
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-02-26 12:22:51 +0900

    포인터 배열

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -1,64 +1,44 @@
 #include <iostream>
 #include <stdio.h>
-#include "func.h"
-#include "common.h"
-
-int g_i = 0;
-
-
-// 정적변수
-static int g_iStatic = 0;
-
-// 외부변수
-
-
-int Test()
-{
-	static int i = 0;	// 정적변수는 한번 컴파일 하였을때 한번만 값이 들어가고 다음부터는 값이 들어가지 않는다
-						// 즉 i 가 한번만 0 으로 set 되고 다음 함수가 호출될때는 set 이 되지않는다.
-	
-	++g_i;
-	++i;
-
-	return i;
-}
+#include <stdlib.h>
 
 int main()
 {
-	g_iStatic = 100;
-	g_iExtern = 500;
-
-	Add(0, 0);
-
-	Test();
-	Test();
-	Test();
-	Test();
-	Test();
-
-	g_i = 0;
-	printf("Test 함수 호출 횟수 : %d\n", g_i);
-
-
-
 	// 포인터 변수
-	// 주소를 저장하는 변수
-	// 자료형 + * 변수명
-	// 해당 포인터에게 전달된 주소를 해석하는 단위
-	int i = 100;
-	float F = 3.f;
+	// 자료형 변수명
+	int* pInt = nullptr;
 
-	int* pInt = (int*)&F;
+	// 주소를 저장
+	char* pChar = nullptr;
+	short* pShort = nullptr;
 
-	// 주소로 접근 (정수표현 방식)
-	// 이안에 저장되어있는 주소값으로 참조 하겠다는것이다 (주소로 들어가는것)
-	i = *pInt;
-	
-	// 주소의 단위 (Byte)
-	100;
+	// 현재 포인터 변수의 크기는 8 바이트다.
+	// 운영체제가 64 비트 이기때문에.
 
-	105;
-	
+	int iSize = sizeof(pInt);
+
+	int i = 0;
+	pInt = &i;
+
+	pInt += 1;
+
+	// pInt 는 int* 변수 이기 때문에, 가리키는 곳을 int 로 해석한다.
+	// 따라서 주소값을 1 증가하는 의미는 다음 int 위치로 접근하기 위해서 sizeof(int) 단위로 증가하게 된다.
+
+	// 포인터와 배열
+	// 배열의 특징
+	// 1. 메모리가 연속적인 구조이다.
+	// 2. 배열의 이름은 배열의 시작 주소이다.
+
+	int iArr[10] = {};
+
+	// int 단위로 접근
+	*(iArr + 0) = 10; // iArr[0] = 10;
+	*(iArr + 1) = 10; // iArr[1] = 10;
+
+	iArr;
+
+
 	return 0;
 }
 
