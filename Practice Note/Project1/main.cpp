d3f2a05d079337418081c83d6de03c0f68bb4633
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-03-07 23:13:40 +0900

    텍스트 무게 재는 함수 제작

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -41,25 +41,111 @@ unsigned int GetLength(const wchar_t* _pstr)
 
 void StrCat(wchar_t* _pDest, unsigned int _iBufferSize, const wchar_t* _pSrc)
 {
-	// 모듈화 : 함수 안에 함수를 인용하여 사용 (불필요한 코드를 없애준다)
 
-	// 이어붙인 최종 문자열 길이가, 목적지 저장 공간을 넘어서려는 경우
+	// 모듈화 : 함수 안에 함수를 인용하여 사용 (불필요한 코드를 없애준다)
 	int iDestLen = GetLength(_pDest);
 	int iSrcLen = GetLength(_pSrc);
 
 	// 예외처리
-	if (_iBufferSize < iDestLen + iSrcLen + 1);	// null 문자 공간까지 계산 + 1 을 해준다
+	// 이어붙인 최종 문자열 길이가, 목적지 저장 공간을 넘어서려는 경우
+	if (_iBufferSize < iDestLen + iSrcLen + 1)	// null 문자 공간까지 계산 + 1 을 해준다
 	{
-		int a = _iBufferSize; 
+		int a = _iBufferSize;
 		assert(nullptr);
-	}
-
+	} 
+	
 	iDestLen;
 
 	for (int i = 0; i < iSrcLen + 1; ++i)
 	{
 		_pDest[iDestLen + i] = _pSrc[i];
 	}
+
+}
+
+int StrCmp(const wchar_t* _LeftStr, const wchar_t* _RightStr)
+{
+
+	// 영어 대, 소문자 아니면 입력 안받게 하고 , 대 소문자 케이스 분류해서 해보장
+	int leftLen = GetLength(_LeftStr);
+	int rightLen = GetLength(_RightStr);
+
+	int iLoop = rightLen;
+	
+
+	if (leftLen < rightLen)
+	{
+		iLoop = rightLen;
+		return -1;
+	}
+	else if(leftLen > rightLen)
+	{
+		iLoop = leftLen;
+		return 1;
+	}
+	
+
+	for (int i = 0; i < iLoop; ++i)
+	{
+		if (_LeftStr[i] < _RightStr[i])
+		{
+			return -1;
+		}
+		else if (_LeftStr[i] > _RightStr[i])
+		{
+			return 1;
+		}
+
+	}
+
+	return 0;
+
+
+
+
+
+
+	
+	/*const int iLMax = GetLength(_LeftStr);
+	const int iRMax = GetLength(_RightStr);
+
+	wchar_t cLeftStr = *_LeftStr;
+	wchar_t cRightStr = *_RightStr;
+
+
+
+	int iIntLeft;
+
+
+	for (int i = 0; i < iLMax; ++i);
+	{
+		_LeftStr[iLMax + i];
+	}
+
+
+	short Result = NULL;
+
+	if (LeftStr < RightStr)
+	{
+		Result = -1;
+	}
+	else if (RightStr < LeftStr)
+	{
+		Result = 1;
+	}
+	else if (LeftStr == RightStr)
+	{
+		Result = 0;
+	}
+	else
+	{
+		assert(nullptr);
+	}*/
+
+	/*switch ()
+	{
+
+	}*/
 
 }
 
@@ -76,33 +162,19 @@ int main()
 	Test(4.14f);
 
 	// 문자열 이어 붙이기
-
-	/*
-
-	wchar_t szString[10] = L"abc";
-
-	문자열의 크기에 넘치는 문자열을 입력시
-	wcscat_s(szString, 10, L"efghijklmnopqrstuvwxyz")
-	*/
-
 	wchar_t szString[100] = L"abc";
 
 	wcscat_s(szString, 100, L"def");
 
-	wchar_t szString1[100] = L"abc";
+	// 문자열 이어 붙이기
+	// 제작한 함수
+	wchar_t szString1[10] = L"abc";
 
-	StrCat(szString1, 100, L"def"); // 왜 이 함수가 오류가 날까? 디버깅 해봐야겠다
-
-	// 과제 함수 직접구현
-
-	// 왼쪽이 더 크다 -1
-	// 오른쪽이 더 크다 1
-	// 서로 같다 0
-	// 첫 글자가 a 가 비교 될때 제일 높게 평가된다
-	// 문자열의 크기의 비교는 아스키 코드로 판별이 된다.
+	StrCat(szString1, 10, L"def"); 
 
 	int iRet = wcscmp(L"abc", L"abd");
 
+	iRet = StrCmp(L"abd", L"abc");
 
 	return 0;
 }
