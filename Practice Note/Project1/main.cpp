50ea7f727d322253938318c036313d5ce28ffaf1
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-03-12 22:18:39 +0900

    Function

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -2,16 +2,32 @@
 
 using namespace std;
 
-int MultiplyBy(int a, int b)
-{
-	int total = a + b;
+void Func1();
+void Func2();
+void Func3();
 
-	return total;
+void Func1()
+{
+	cout << "Func1" << endl;
+
+	Func2();
+}
+
+void Func2()
+{
+	cout << "Func2" << endl;
+
+	Func3();
+}
+
+void Func3()
+{
+	cout << "Func3" << endl;
 }
 
 int main()
 {
-	
+	Func1();
 
 	return 0;
 }
