9c3529779911834b83376da91e8e0e592ee0f65f
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-03-13 15:54:50 +0900

    Text "RPG"

--- Practice Note/Project1/Func.h
+++ Practice Note/Project1/Func.h
@@ -1,11 +1,25 @@
 #pragma once
-// 코드를 관리하기위해 해더와 그에따른 구현 CPP 를 만든다
 
-// int data 영역
 
-int Add(int a, int b);
 
-int Sub(int a, int b);
 
-int Mul(int a, int b);
+void EnterLobby();
+void SelectPlayer();
+void EnterField();
+void CreateRandomMonster();
+void EnterBattle();
+
+enum PlayerType
+{
+	PT_Knight = 1,
+	PT_Archer = 2,
+	PT_Mage = 3,
+};
+
+enum MonsterType
+{
+	MT_Slime = 1,
+	MT_Orc =2 ,
+	MT_Skeleton = 3,
+};
 
