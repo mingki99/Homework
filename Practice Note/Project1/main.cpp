e513fdff42c695e18828c4503013e09bcc240a48
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-03-15 23:47:25 +0900

    포인터

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-
+void SetHP(int* hp)
+{
+	*hp = 100;
+}
 
 
 
 int main()
 {
-	int* ptr;
+	int HP = 1;
+	SetHP(&HP);
+
+	printf("%d\n", HP);
+
+	int hp;
+
+	// 타입의 불일치
+	__int64* a;
 	
 	return 0;
 }
