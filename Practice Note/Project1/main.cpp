c491438cd9d9b978a8705e2ee562c34a8b6eaf0f
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-03-26 14:26:10 +0900

    포인터마무리

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -4,69 +4,48 @@
 
 using namespace std;
 
-// 오늘의 주제 : 다차원 배열
+// 오늘의 주제 : 포인터 마무리
 
+// 1) 포인터 vs 배열 2탄
+// 2) 주의사항 (마음가짐)
+
+int* TestPointer()
+{
+	int a = 1;
+
+	return &a;
+}
 
 int main()
 {
-	int a[10] = { 1, 2, 3 };
+	int* p;
+
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+	p = arr;
+
+	cout << p[0] << endl;
+	cout << arr[0] << endl;
+	cout << p[5] << endl;
+	cout << arr[5] << endl;
+	cout << *p << endl;
+	cout << *arr << endl;
+	cout << *(p + 3) << endl;
+	cout << *(arr + 3) << endl;
+
+	int arr2[2][2] = { {1,2}, {3,4} };
+
+	int(*p2)[2] = arr2;
+	cout << (*p2)[0] << endl;
+	cout << (*p2)[1] << endl;
+	cout << (*(p2+1))[0] << endl;
+	cout << (*(p2 + 1))[1] << endl;
 	
-	// [4] [2] [3] [4] [1] << 0층
-	// [1] [1] [5] [2] [2] << 1층
 
-	int first[5] = { 4, 2, 3, 4, 1 };
-	int second[5] = { 1, 1, 4, 2, 2 };
-
-	// 일차원은 왼쪽부터 
-	int aprtment2D[2][5] = { { 4, 2, 3, 4, 1 }, { 1, 1, 5, 2, 2 }  };
-	
-	for (int floor = 0; floor < 2; ++floor)
-	{
-		for (int room = 0; room < 5; ++room)
-		{
-			int num = aprtment2D[floor][room];
-
-			// cout << num << "  ";
-		}
-		// cout << endl;
-	}
-
-	int aparment1D[10] = { 4, 2, 3, 4 ,1, 1, 1, 5, 2, 2 };
-
-
-	for (int floor = 0; floor < 2; ++floor)
-	{
-		for (int room = 0; room < 5; ++room)
-		{
-			int index = (floor * 5) + room;
-			int num = aparment1D[index];
-
-			// cout << num << "  ";
-		}
-		// cout << endl;
-	}
-
-
-	// 2차 배열 활요 영역
-	int map[5][5] =
-	{
-		{ 1, 1, 1, 1, 1},
-		{ 1, 0, 0, 0, 1},
-		{ 1, 0, 0, 0, 1},
-		{ 1, 0, 0, 0, 1},
-		{ 1, 1, 1, 1, 1},
-	};
-
-	for (int y = 0; y < 5; ++y)
-	{
-		for (int x = 0; x < 5; ++x)
-		{
-			int info = map[y][x];
-
-			cout << info << "  ";
-		}
-		cout << endl;
-	}
+	cout << p2[0][0] << endl;
+	cout << p2[0][1] << endl;
+	cout << p2[1][0] << endl;
+	cout << p2[1][1] << endl;
 
 
 
