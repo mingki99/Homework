2826b23d740e836b5b86a6b88a55a934ca402093
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-08 16:58:17 +0900

    달팽이 문제 풀이

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -10,27 +10,105 @@ const int MAX = 100;
 int board[MAX][MAX] = {};
 int N = 0;
 
-int iCount = 1;
+
 
 void PrintBoard()
 {
 	for (int y = 0; y < N; y++)
 	{
 		for (int x = 0; x < N; x++)
-		{
-			board[y][x] = iCount;
-			cout << board[y][x] << " ";
-			iCount ++;
+		{	
+			cout << setfill('0') << setw(2) << board[y][x] << " ";
 		}
 		cout <<endl;
 	}
 }
 
+enum DIR
+{
+	RIGHT = 0,
+	DOWN = 1,
+	LEFT = 2,
+	UP = 3, 
+};
+
+bool CanGo(int y, int x)
+{
+	if (y < 0 || y >=N)
+		return false;
+	if (x < 0 || x >=N)
+		return false;
+	if (board[y][x] != 0)
+		return false;
+	return true;
+}
+
 void SetBoard()
 {
-	for (int i = 0; i < N; ++i)
+	int dir = RIGHT;
+
+	// Âï¾îÁÙ ¹øÈ£
+	int num = 1;
+
+	int y = 0;
+	int x = 0;
+
+	while (true)
 	{
-		
+		board[y][x] = num;
+
+		if ( num == N*N)
+			break;
+
+		int nextY;
+		int nextX;
+
+		switch (dir)
+		{
+		case RIGHT:
+			nextY = y;
+			nextX = x +1;
+			break;
+		case DOWN:
+			nextY = y + 1;
+			nextX = x;
+			break;
+		case LEFT:
+			nextY = y;
+			nextX = x - 1;
+			break;
+		case UP:
+			nextY = y - 1;
+			nextX = x;
+			break;
+		}
+
+
+		if (CanGo(nextY, nextX))
+		{
+			y = nextY;
+			x = nextX;
+			num ++;
+		}
+		else
+		{
+			switch (dir)
+			{
+			case RIGHT:
+				dir = DOWN;
+				break;
+			case DOWN:
+				dir = LEFT;
+				break;
+			case LEFT:
+				dir = UP;
+				break;
+			case UP:
+				dir = RIGHT;
+				break;
+			}
+		}
+
 	}
 	
 	// 01 02 03 04 05
