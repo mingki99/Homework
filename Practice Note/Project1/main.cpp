bcb15c8e14695ca15cda433aaac5ce85b25ca078
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-16 23:36:38 +0900

    3052

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -2,37 +2,34 @@
 
 using namespace std;
 
-struct Person
-{
-	int age;
-	double weight;
-	double height;
 
-};
-
-void PersonData(Person* person)
-{
-	printf("나이를 입력하십시오\n");
-	cin >> person->age;
-
-	printf("몸무게를 입력하십시오\n");
-	cin >> person->weight;
-
-	printf("키를 입력하십시오\n");
-	cin >> person->height;
-
-	person->age = person->age + 1;
-}
 
 int main()
 {
-	Person p1;
-	Person p2;
 
-	PersonData(&p1);
+	int a[10] = {};
+	int b[42] = {};
 
-	printf(" 나이 : %d 몸무게 : %0.1f 키 : %0.1f 입니다 \n", p1.age, p1.weight, p1.height);
+	int count = 0;
 
+	for (int i = 0; i <= 9; ++i)
+	{
+		cin >> a[i];
+
+		b[(a[i]%42)] = 1;
+		
+	}
+
+
+	for (int i = 0; i <= count; ++i)
+	{
+		if (1 == b[i])
+		{
+			count++;
+		}
+	}
+
+	printf ("%d",count);
 
 
 
