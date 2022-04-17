4c440334ab4fd3bb325587df8bcb5cb31ffd32d8
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-17 22:13:25 +0900

    class 활용하여 만들기

--- I Make Projact/RPGProject_CPP/main.cpp
+++ I Make Projact/RPGProject_CPP/main.cpp
@@ -26,50 +26,44 @@ using namespace std;
 class Inventory
 {
 public:
-	Inventory() {cout << "Inventory() " << endl; }
+	Inventory() { cout << "Inventory() " << endl; }
 	Inventory(int size) { cout << "Inventory(int size) " << endl; }
 
 	~Inventory() { cout << "~ Inventory() " << endl; }
 };
 
-class Player
+class Knight 
 {
 public:
-	Player() {}
-	Player(int id) {}
-};
-
-// Is - A (Knight Is - A Player? 기사는 플레이어냐 ) OK -> 상속관계
-// 
-// Has - A (Knight Has - A Inventory? 기사는 인벤토리를 가지고 있냐?) OK -> 포함관계
-
-class Knight : public Player
-{
-public:
-	Knight() : Player(1), _hp(100), _inventory(20), _hpref(_hp), _hpConst(100)	// 초기화 리스트를 사용하게 된다면 선처리 할 때 지정된 값으로 생성된다.
+	
+	Knight()	
 		/*
 		선처리 영역
 
+		* 생성자 내부에서 초기화할시 *
 		_inventory = Inventory() 생성자 실행 -> 선처리 영역에서 이런식의 구문으로 생성자를 만들어준다.
-
+		
 		*/
 	{
-		_hp = 100;
+		_hp = 100;			// 일반 변수는 별 차이나지 않는다.
+		// _hpref = _hp;	// 이 두개는 생성자에서 초기화 시켜주지 못한다 즉, 동적으로 사용할 수 가 없다.
+		// _hpConst = 100;	
 
-		// _hpref  = _hp;	생성된 이후에 초기화 할 순 없다.
-		//_hpConst = 100;	
-		
-		// 생성자 내부에서 Inventory(int size)를 호출 하고싶었지만 이렇게 하면 생성자가 2개 호출된다.
-		// _inventory = Inventory(20);
+		//생성자 내부에서 Inventory(int size)를 호출 하고싶었지만 이렇게 하면 생성자가 2개 호출된다.
+		_inventory = Inventory(20);
 	}
+
 
 public:
 	int _hp;	// 초기화를 하지 않을시 쓰레기 값이 들어가있다
+
+	// Has - A (Knight Has - A Inventory? 기사는 인벤토리를 가지고 있냐?) OK -> 포함관계
 	Inventory _inventory;	// 인벤토리가 생성자에서 생성될때 같이 생성된다.
 
+
 	// 정의와 동시에 초기화가 필요하다.
-	int& _hpref;
-	const int _hpConst;
+	int& _hpref = _hp;	// 초기화 리스트를 사용하지않는 참조변수는 즉시 초기화 시켜줘야한다.
+	const int _hpConst = 0; // const는 이미 상수로 지정이 되어서 대입&초기화 시킬수 없다.
 };
 
 
@@ -77,14 +71,9 @@ int main()
 {
 	Knight k;
 
-	cout << k._hp <<endl;	// -858993460 출력
-
-	// -858993460값이 기에 ture가 된다.
-	if (k._hp < 0)
-	{
-		cout << "Knight is Dead" << endl;
-	}
-
+	printf("\t Knight 체력 : %d\n", k._hp);
+	printf("\t Knight 주소에 담긴체력 : %d\n", k._hpref);
+	printf("\t Knight 변하지않는 상수 값 : %d\n", k._hpConst);
 
 	return 0;
 }
--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -2,35 +2,80 @@
 
 using namespace std;
 
+class point
+{
+public:
+	point() : X(0), Y(0)
+	{
+
+	}
+
+	void SetX(int x = 0)
+	{
+		printf("X 좌표를 입력하세요\n");
+
+		cin >> x;
+		if (0 < x && 10 > x)
+		{
+			X = x;
+		}
+		else
+		{
+			x = 0;
+			X = x;
+		}
+	}
+
+	void SetY(int y = 0);
+		/*{
+			printf("X 좌표를 입력하세요");
+
+			cin >> y;
+			if (0 < y && 10 > y)
+			{
+				Y = y;
+			}
+			else
+			{
+				Y = 0;
+				Y = y;
+			}
+		}*/
+	int GetX(){ return X;}
+	int GetY() { return Y;}
+
+public:
+	int X = 0;
+	int Y = 0;
+};
+
+void point::SetY(int y)
+{
+	printf("Y 좌표를 입력하세요\n");
+
+	cin >> y;
+	if (0 < y && 10 > y)
+	{
+		Y = y;
+	}
+	else
+	{
+		Y = 0;
+		Y = y;
+	}
+}
 
 
 int main()
 {
+	point p1;
+	p1.SetX();
 
-	int a[10] = {};
-	int b[42] = {};
+	point p2;
+	p2.SetY();
 
-	int count = 0;
-
-	for (int i = 0; i <= 9; ++i)
-	{
-		cin >> a[i];
-
-		b[(a[i]%42)] = 1;
-		
-	}
-
-
-	for (int i = 0; i <= count; ++i)
-	{
-		if (1 == b[i])
-		{
-			count++;
-		}
-	}
-
-	printf ("%d",count);
-
+	printf("좌표는 (%d, %d) 입니다 \n", p1.GetX(), p2.GetY());
+	
 
 
 	return 0;
