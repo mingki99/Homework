8e3a84eee4dcb306f5a2bf4e9dc7168fd53ef369
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-19 18:55:37 +0900

    대입연산자와 연산자, 복사 생성자

--- I Make Projact/RPGProject_CPP/main.cpp
+++ I Make Projact/RPGProject_CPP/main.cpp
@@ -2,91 +2,54 @@
 
 using namespace std;
 
-// 오늘의 주제 : 객체지향 마무리
-
-// 1) struct vs class
-
-// C++에서는 strut나 calss나 종이 한 장 차이다.
-// struct는 기본 접근 지정자가 public이고, class는 parivate이다.
-// 왜 이렇게 했을까? C에서 파생되어 발전했기 떄문에, 호환성을 지키기 위함
-// -> struct는 그냥 구조체 (데이터 묶음)을 표현하는 용도
-// -> struct
-
-
-struct TestStruct
-{
-
-};
-
-
-class TestClass
+class Test
 {
 public:
-
-};
-
-// 2) static 변수, static 함수 (staic = 정적 = 고정된)
-// 클래스를 대표하는 변수와 함수.
-
-class Marine
-{
-public:
-	void TakeDamage(int damage)
+	Test()
 	{
-		_hp -= damage;
+		printf("기본생성자 호출\n");
+	}
+	Test(const Test& test)
+	{
+		printf("복사 생성자 호출\n");
 	}
 
-	static void TEST()
+	// 생성자 아닙니다 [연산자] 입니다~!
+	Test& operator = (const Test& test)
 	{
-		// 클래스 안에있는 일종의 전역함수 이기에 _hp는 어떤 Marine의 _hp를 건들건지 명시적이지 못 한다.
-		// _hp = 100	
+		printf("복사 대입 연산자 실행\n\n\n");
+		return *this;
 	}
 
-public:
+	int GetClass() 
+	{
+		printf("GetClass 함수 실행 \n"); 
+		return this->_temp; 
+	}
 
-	int _hp;
-	static int s_attack;	// 완전 다른 메모리에 잡혀있다. 설계도 상으로만 존재
+private:
+
+int _temp;
 };
 
-// static 변수는 어떤 메모리?
-// 초기화 하면 .data
-// 초기화 안하면 .bss
-
-int Marine::s_attack = 0;
-
-int Generateld()
+int isTemp(Test* T)	// 참조냐 복사냐!
 {
-	// 생명주기 : 프로그램 시작 / 종료 (메모리에 항상 올라가 있음)
-	// 가시범위 : 
-	
-	// 정적 지역 객체
-	// 변수의 초기화는 함수의 호출로 인해 딱 한번 실행되며 변수가 프로그램 끝날때 까지 변수는 살아있다.
-	static int s_id = 1;	
-
-	return s_id++;
+	int temp = T->GetClass();
+	return temp;
 }
+
 
 int main()
 {
-	Marine m1;
-	m1._hp = 35;
-	m1.TakeDamage(10);
-	
+	Test t1;	// 기본생성자 호출
 
-	Marine::s_attack = 6;
-	// m1.s_attack = 6;
+	Test t2(t1);	// 복사 생성자 호출
 
-	Marine m2;
-	m2._hp = 40;
-	m2.s_attack = 6;
+	Test t3 = t1;	// ★복사 생성자 호출★
 
-	// 마린 공격력 업그레이드 완료! (Academy 업그레이드 끝)
-	m1.s_attack = 7;
-	m2.s_attack = 7;
+	t3 = t1;	// 복사 대입 연산자 호출
 
-	// 스택 아님 .data 영역
-	static int id = 10;
-	int a = id;
+	int temp = isTemp(&t3);
 
 	return 0;
 }
