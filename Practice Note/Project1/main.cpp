bbc2eaeac8a623a048920825a9db4651bc27aab1
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-21 08:55:14 +0900

    등차수열 1065번

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -2,48 +2,73 @@
 
 using namespace std;
 
+// 양의 정수의 각 자리 마다 등차수열이 되는 횟수 를 입력해야한다.
+
+// 101 이면 101 이하의 Hansu() 가 나와야한다.
+
+// 125
+bool  Hansu(int a)
+{	
+	int b = a/10;
+
+	int Gongcha = b%10 - a%10;
+
+	int temp = a % 10;
 
 
-class Handle
-{
-public:
-	Handle()
+	// 1, 2, 3, 4 순서대로 인자를 넘겨 줘야한다 4, 3 -> 3, 2 -> 
+
+	while (1)
 	{
-		printf("Handle spawn \n");
-	}
-};
+		
+		
+		a = a / 10;
 
-class Car	: private Handle
-{
-public:
-	Car()
-	{
-		printf("Car Spawn\n");
-	}
-};
+		int temp2 = a % 10;
 
-class RacingCar : public Car
-{
-public:
-	RacingCar()
-	{
-		printf("RasingCar Spawn\n");
-	}
-};
+		if (a == 0)
+		{
+			break;
+		}
 
-class SpeedCar :public RacingCar
-{
-public:
-	SpeedCar()
-	{
-		printf("SpeedCar Spawn\n");
+
+		if (Gongcha == temp2 - temp)
+		{
+			
+			temp = temp2;
+		}
+		else
+		{
+			return false;
+		}
+	
 	}
-};
+
+	return true;
+
+
+}
 
 
 int main()
 {
-	SpeedCar pc1;
+	int a;
+
+	cin >> a;
+
+	int hansu = 0;
+
+	for (int i = 1; i <= a; ++i)
+	{
+		if (Hansu(i))
+		{
+			hansu++;
+		}
+	}
+	
+	
+
+	printf("%d", hansu);
 	
 	
 	return 0;
