7713e8b9a4f6781dc31a4ca3fe2880e710c556f3
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-28 16:00:09 +0900

    2292번문제 풀이

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -3,74 +3,26 @@
 using namespace std;
 
 
-// 불 자료형과 char 자료형을 결합시켜서 넣어주자
-
-int GroupWord()
-{
-
-		char s[100];
-		cin >> s;	// abcabc a가 걸리면 false
-
-		char tempC[26];
-		bool Check[26] = { 0, };
-
-
-
-		for (int i = 0; i < 26; ++i)
-		{
-			tempC[i] = i + 97;
-		}
-
-
-		// 조건
-		// 입력된 숫자가 반복이 되었는가 체크.
-		// 알파벳 쭉 넣을까?..
-
-
-		for (int i = 0; s[i] != '\0'; i++)
-		{
-			for (int j = 0; j < 26; j++)
-			{
-				if (tempC[j] == s[i])
-				{
-					if (tempC[j] == s[i - 1])
-					{
-						break;
-					}
-					else if (!Check[j])
-					{
-						Check[j] = true;
-						break;
-					}
-					else
-					{
-						return 1;
-					}
-
-				}
-
-			}
-		}
-	
-	return 0;
-	
-}
-
+// 규칙 6의 배수 찾기.
 
 int main()
 {
-	int PlayCount = 0;
+	int A;
+	
+	cin >> A;
 
-	cin >> PlayCount;
 
-	int score =  PlayCount;
 
-	for (int i = 0; i < PlayCount; i++)
+	int startPoint = 1;
+
+	int count = 1;
+
+	for (count; startPoint < A ; ++count)
 	{
-		score -= GroupWord();
+		startPoint = startPoint + (6 * count);
 	}
-
-	cout << score <<endl;
+	
+	cout << count << endl;
 
 
 	return 0;
