0390d8ecf3a4504d242a93c1a7fd3accbe52e38a
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-04-29 09:57:33 +0900

    직접구현한 콜백 기능

--- I Make Projact/RPGProject_CPP/main.cpp
+++ I Make Projact/RPGProject_CPP/main.cpp
@@ -5,81 +5,110 @@ using namespace std;
 // 원하는 타이밍에 함수를 호출 하는 것.
 
 
-class Item
+class Player
 {
 public:
 
 public:
-	int _itemid = 0;
-	int _rarity = 0;
-	int _ownerid = 0;
+	int _hp;
+	int _attack;
+	int _numid;
 };
 
-// Functor 생성, 지정된 아이디와 인자를 받은 아이디를 검사해준다.
-
-class FindByOwnerID	
+class Knight : public Player
 {
 public:
-	bool operator() (const Item* item)
-	{
-		return item->_ownerid==_ownerid;
-	}
-
-public:
-	int _ownerid;
+	
 };
 
 
-// Functor 생성, 지정된 아이템의 값과 인자를 받은 레어값을 검사해준다.
-class FindByRarity	
+class Archer : public Player
 {
 public:
-	bool operator() (const Item* item)
+	
+};
+
+// Functor 생성
+class FunctorGetHp
+{
+public:
+	bool operator () (const Player* player)
 	{
-		return item->_rarity == _rarity;
+		return player->_hp == _hp;
 	}
 
 public:
-	int _rarity;
+	int _hp;
+};
+
+class FunctorGetAttack
+{
+public:
+	bool operator () (const Player* player)
+	{
+		return player->_attack == _attack;
+	}
+
+public:
+	int _attack;
 };
 
 
-template <typename T>	// 템플릿을 이용하여 사용
-Item* FindItem(Item items[], int itemCount, T selector)	// Functor들을 넘겨준다
+template<typename T>
+Player* FindState(Player players[], const int FindCount, T seleter)
 {
-	for (int i = 0; i < itemCount; i++)
+	for (int i = 0; i < FindCount + 1; ++i)
 	{
-		Item* item = &items[i];
-		
-		if(selector(item))
-			return item;
+		Player* player = &players[i];
+
+		if(seleter(player))
+		{ 
+			player->_numid = i;	//  몇번 째인지 저장
+			return player;
+		}
+			
 	}
+	
+
 }
+
 
 
 
 int main()
 {
+	const int spawnNum = 5;
 
-	// items 10개 생성
-	Item items[10];
-	items[3]._ownerid = 100;
-	items[8]._rarity = 2;
+	FunctorGetHp FUgetHp;
 
+	// attack이 10인 친구를 찾아낸다.
+	FunctorGetAttack FUgetAttack;
+	FUgetAttack._attack = 10;
 
-	// 람다를 이용하면 직접 functor를 만들지 않고 함수를 사용 할 수 있다.
-	// functor 2개생성
-	FindByOwnerID functorOwner;
-	functorOwner._ownerid = 100;	// 체력이 100인 items를 찾는다
+	
+	
+	Knight k[spawnNum];	// 기사 생성
+	k[3]._attack = 10;
+	 
+	// 유의사항
+	// 부모는 자식이 될 수 없다.
+	// 자식은 부모가 될 수 있다.
+	
+	// attack이 10인 knight를 찾아내 출력 해보자!
+	Player* player = FindState(k, spawnNum, FUgetAttack);
 
-	FindByRarity functorRarity;
-	functorRarity._rarity = 1;		// rariy가 1인 items를 찾는다.
+	printf("knight의 %d 번째의 공격력은 : %d 이다\n",player->_numid,player->_attack);
 
-	// FindOwnerID로 실행
-	Item* item = FindItem(items, 10, functorOwner);	
-	// FindByRarity로 실행
-	Item* item2 = FindItem(items, 10, functorRarity);
+	// ------------------ archer 생성 --------------------
 
+	FUgetHp._hp = 80;
+
+	Archer archer[5];
+	archer[4]._hp = 80;
+	
+	player = FindState(archer, spawnNum,FUgetHp);
+	
+	printf("Archer의 %d 번째의 체력은 : %d 이다 \n",player->_numid ,player->_hp);
 
 	return 0;
 }
