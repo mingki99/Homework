c90f7000456dfd3b0ceacb7093bc1cd28edb9827
ming <dkwnajsquf@naver.com>
2022-04-30 15:42:08 +0900

    1193번 문제 해결

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
-enum rull
+enum add
 {
 	RIGHT = 0,
 	LEFT = 1,
+	ADDRIGHT = 3,
+	ADDLEFT = 4,
 };
-
 
 int main()
 {
@@ -16,33 +17,71 @@ int main()
 
 	int num;
 
-	bool Rull = 0;
+	int temp = 1;
+	int lefttemp = 1;
+
+	add addLocation = RIGHT;
+
 
 	cin >> num;	// 14
 
-	for (int i = 0; i < num; ++i)
+	// 1/1 ->  1/2 ->  2/1 ->  3/1 ->  2/2 ... 3/3... 4/4   5/5
+	//	1		2		3		4		5		13	   25	 42
+	//									4		8	   12    16
+
+
+
+	for (int i = 1; i < num; ++i)	// 한번 더 실행되야 계산이 맞으니까
 	{
+
+
 		
-		switch (Rull)
+		// 어떤 조건을 넣어야지 원하는 값이 나올까?
+
+			
+
+		switch (addLocation)
 		{
 		case RIGHT:
 			right += 1;
+			temp = right;
+
+			addLocation = ADDLEFT;
 			break;
 		case LEFT:
 			left += 1;
-			break;
-		}
-		
+			temp = left;
 
+			addLocation = ADDRIGHT;
+			break;
+
+		case ADDRIGHT:
+			right += 1;
+			left -= 1;
+
+			if (right == temp)
+			{
+				addLocation = RIGHT;
+				break;
+			}
+			break;
+
+		case ADDLEFT:
+			left += 1;
+			right -= 1;
+
+			if (left == temp)
+			{
+				addLocation = LEFT;
+				break;
+			}
+			break;
 		
-		if (right == left)
-		{
-			Rull = NOT;
-		}
-		
+		} 
 
 	}
 
+	cout << left << '/' << right << endl;
 
 	return 0;
 }
