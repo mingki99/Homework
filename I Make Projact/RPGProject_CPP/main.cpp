7414827927e526fdffe58348a48b83d080d04854
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-05-04 20:23:21 +0900

    List

--- I Make Projact/RPGProject_CPP/main.cpp
+++ I Make Projact/RPGProject_CPP/main.cpp
@@ -1,114 +1,126 @@
 #include <iostream>
 using namespace std;
+#include <list>
 
-// 오늘의 주제 : 콜백 (Callback)
-// 원하는 타이밍에 함수를 호출 하는 것.
+// 오늘의 주제 : list
+
+// vector : 동적 배열
+// [               ]
 
 
-class Player
+
+class Node
 {
 public:
+	Node*	_next;	// node의 주소값을 넣을것이기에 class node를 사용한다.
+	Node*	_prev;
+	int		_data;
 
-public:
-	int _hp;
-	int _attack;
-	int _numid;
-};
-
-class Knight : public Player
-{
-public:
-	
 };
 
 
-class Archer : public Player
-{
-public:
-	
-};
+// 단일 / 이중 / 원형
+// list : 연결 리스트
 
-// Functor 생성
-class FunctorGetHp
-{
-public:
-	bool operator () (const Player* player)
-	{
-		return player->_hp == _hp;
-	}
+// 나란히 나열되있는 배열이 아니라 서로의 다음포인터를 가지고있는 data들이다. 
+// 
+// 칸단위를 노드라고한다
+// 
+// 단일 리스트
+// [1] -> [2] -> [3] -> [4] -> [5]
 
-public:
-	int _hp;
-};
+// 이중 리스트
+// [1] <-> [2] <-> [3] <-> [4] <-> [5] <-> [My_Head : end()]	end()함수를 사용할때 있는 마지막 값이 따로 변수로가지고있다.
 
-class FunctorGetAttack
-{
-public:
-	bool operator () (const Player* player)
-	{
-		return player->_attack == _attack;
-	}
-
-public:
-	int _attack;
-};
-
-
-template<typename T>
-Player* FindState(Player players[], const int FindCount, T seleter)
-{
-	for (int i = 0; i < FindCount + 1; ++i)
-	{
-		Player* player = &players[i];
-
-		if(seleter(player))
-		{ 
-			player->_numid = i;	//  몇번 째인지 저장
-			return player;
-		}
-			
-	}
-	
-
-}
-
+// 원형 리스트
+// [1] <-> [2] <-> [3] <-> [4] <-> [5] <->
 
 
 
 int main()
 {
-	const int spawnNum = 5;
+	// list (연결 리스트)
+	// - list의 동작 원리
+	// - 중간 삽입/삭제		= 전부 O에 1의 비용을 들이고 삭제된다.
+	// - 처음/끝 삽입/삭제	= 전부 O에 1의 비용을 들이고 삭제된다.
+	// - 임의 접근 (i번째 데이터는 어디잇습니까?)			= 임으
 
-	FunctorGetHp FUgetHp;
+	list<int> li;
 
-	// attack이 10인 친구를 찾아낸다.
-	FunctorGetAttack FUgetAttack;
-	FUgetAttack._attack = 10;
+	list<int>::iterator itRemember;
+
+	for (int i = 0; i < 100; i++)
+	{
+		if ( i == 50)
+		{	
+			// 리스트를 초기화 할 때 지울 값을 저장한다.
+			itRemember = li.insert(li.end(), i);	// 50을 iterator로 기억하고있다.
+		}
+		else
+		{
+			li.push_back(i);
+		}
+		
+		
+	}
 
 	
-	
-	Knight k[spawnNum];	// 기사 생성
-	k[3]._attack = 10;
-	 
-	// 유의사항
-	// 부모는 자식이 될 수 없다.
-	// 자식은 부모가 될 수 있다.
-	
-	// attack이 10인 knight를 찾아내 출력 해보자!
-	Player* player = FindState(k, spawnNum, FUgetAttack);
+	li.push_front(10);	// 
 
-	printf("knight의 %d 번째의 공격력은 : %d 이다\n",player->_numid,player->_attack);
+	int size = li.size();
+	// li.capacity() 없음	실시간으로 데이터를 만들어주고 연결해준다.
 
-	// ------------------ archer 생성 --------------------
+	int first = li.front();
+	int last = li.back();
 
-	FUgetHp._hp = 80;
-
-	Archer archer[5];
-	archer[4]._hp = 80;
 	
-	player = FindState(archer, spawnNum,FUgetHp);
-	
-	printf("Archer의 %d 번째의 체력은 : %d 이다 \n",player->_numid ,player->_hp);
+
+	list<int>::iterator itBegin = li.begin();
+	list<int>::iterator itEnd = li.end();
+
+	// li[3] = 10;	접근 불가능
+	// li + 10;		불가능
+
+	int* ptrBegin = &(li.front());
+	int* ptrEnd = &(li.back());
+
+	// list<int>::iterator itTest1 = --itBegin;	begin에서 한칸뒤로(end)로 이동은 불가능
+	list<int>::iterator itTest1 = --itEnd;
+	// list<int>::iterator itTest1 = ++itEnd;	end에서 한칸뒤로(begin)으로 이동은 불가능
+
+	for (list<int>::iterator it = li.begin(); it != li.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+
+
+	li.insert(itBegin, 100);	// 지정한 위치 100 넣기.
+
+	li.erase(li.begin());	// 맨 앞 삭제
+
+	li.pop_front();	// 맨 앞 삭제
+
+	li.remove(10);	// 인자값과 같은 배열값을 전부 삭제한다 
+
+
+
+	// 임의접근은 가능하지만 중간삽입 삭제는 빠르다?
+	// 
+	// 임의접근하여 삽입삭제 하는 것은 느리다.
+	// 50번 인덱스 데이터를 삭제
+	list<int>::iterator it = li.begin();
+	for(int i =0; i< 50; i++)
+		++it;
+
+	li.erase(it);	// iterartor로 되있는 50은 빠르게 처리가 가능하다.
+
+	li.erase(itRemember);	// 그리하여 삽입 삭제한 값을 미리 들고있어서 바로 넣어줄 수 있다.
+
+	// li.erase(50); 
+
+
+
+
 
 	return 0;
 }
--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -1,189 +1,52 @@
 #include <iostream>
+
 using namespace std;
-
-#include <vector>
-// 오늘의 주제 : vector
-
-template<typename T>
-class Iterator
-{
-public:
-	Iterator() : _ptr(nullptr)
-	{
-	}
-
-	Iterator(T* ptr) : _ptr(ptr)
-	{
-
-	}
-
-	Iterator operator+(const int count)
-	{
-		Iterator temp = *this;
-		temp._ptr += count;
-		return temp;
-	}
-
-	Iterator operator-(const int count)
-	{
-		Iterator temp = *this;
-		temp._ptr -= count;
-		return temp;
-	}
-
-	Iterator& operator++()
-	{
-		_ptr++;
-		return *this;
-	}
-
-	Iterator operator++(int)
-	{
-		Iterator temp = *this;
-		_ptr++;
-		return temp;
-	}
-
-	Iterator& operator--()
-	{
-		_ptr--;
-		return *this;
-	}
-
-	Iterator operator--(int)
-	{
-		Iterator temp = *this;
-		_ptr--;
-		return temp;
-	}
-
-	bool operator==(const Iterator& right)
-	{
-		return _ptr == right._ptr;
-	}
-
-	bool operator!=(const Iterator& right)
-	{
-		return !(*this == right);	// == 의 만든 오퍼레이트를 활용해 !=을 만들어줬다.
-	}
-
-	T& operator*()
-	{
-		return *_ptr;
-	}
-
-
-
-
-public:
-	T* _ptr;
-};
-
-
-
-template<typename T>
-class Vector
-{
-public:
-	Vector() : _data(nullptr), _size(0), _capacity(0)
-	{
-
-	}
-
-	~Vector()
-	{
-		if (_data)
-		{
-			delete[] _data;
-		}
-	}
-
-	void push_back(const T& val)
-	{
-		if (_size == _capacity)
-		{
-			// 증설 작업
-			int newCapacity = static_cast<int>(_capacity * 1.5);
-			if(newCapacity == _capacity)
-				newCapacity++;
-
-			reserve(newCapacity);
-		}
-
-		// size = 3
-		// [1] [2] [3]  
-		// 데이터 저장
-		_data[_size] = val;
-
-		// 데이터 개수 증가
-		_size++;
-	}
-
-	void reserve(int capacity)
-	{
-		_capacity = capacity;
-		T* newData = new T[_capacity];
-
-		// 데이터 복사
-		for (int i = 0; i < _size; ++i)
-		{
-			newData[i] = _data[i];
-		}
-
-		// 기존에 있던 데이터 날린다.
-		if(_data)
-			delete[] _data;
-
-		_data = newData;
-	}
-
-	// 데이터를 밀어넣는 처리까지 하려면 실제 값을 건드려야 하기에 레퍼런스로 지정해놓는다.
-	T& operator[](const int pos) {return _data[pos]; }
-
-
-	void clear() {_size = 0;}
-	int size() {return _size;}
-	int capacity() {return _capacity; }
-
-
-public:
-	typedef Iterator<T>iterator;	// vector::iterator
-
-	iterator begin() {return iterator(&_data[0]); }
-	iterator end() { return begin() + _size;}
-
-private:
-	T* _data;
-	int _size;
-	int _capacity;
-};
 
 
 int main()
 {
-	vector<int> v;
-	v.reserve(100);
+	int i;
 
-	for (int i = 0; i < 100; ++i)
+	cin >> i;
+	while (i)
 	{
-		v.push_back(i);
-		cout << v.size() << " " << v.capacity() << endl;
+		int W;	// 30
+		int H;	// 50
+		int N;	// 150 번째
+
+		// 3050호 가 나와야한다.
+
+
+		// W / N = 1 ㅇ상일 경우 02 호로 전환 = 2일경우 03
+
+		cin >> W >> H >> N;
+
+		int hundred = N % W;
+		if (!hundred)
+			hundred += W;
+
+		int tenth = N / W;
+		if (!tenth)
+			tenth++;
+		else
+		{
+			tenth++;
+		}
+
+
+		if (tenth <= 10)
+		{
+			cout << hundred << "0" << tenth << endl;
+		}
+		else
+		{
+			cout << hundred << tenth << endl;
+		}
+
+
+		--i;
 	}
 
-	for (int i = 0; i < v.size(); ++i)
-	{
-		cout << v[i] << endl;
-	}
-
-	cout << "----------------" <<endl;
-
-	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-	{
-		cout << (*it) <<endl;
-	}
-
-	v.clear();	// capasity는 남겨두고 size만 0으로 만들어준다.
 
 	return 0;
 }
-
