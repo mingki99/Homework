8e50388923ffcf205a112fec185d13edb4568eea
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-05-09 13:47:19 +0900

    set, multiset, multimap

--- I Make Projact/RPGProject_CPP/main.cpp
+++ I Make Projact/RPGProject_CPP/main.cpp
@@ -3,64 +3,116 @@ using namespace std;
 #include <list>
 #include <vector>
 #include <deque>
+#include <map>
 
-
-// 오늘의 주제 : deque
-
-// vector : 동적 배열
-// [          ]
-
-// list : 이중 연결 리스트
-// [ ] <-> [ ] <-> [ ] <-> []
-
-// deque : doble-ended queue 데크
-// [   ]
-// [   ]
-
+// 오늘의 주제 : set, multimap, multiset
+#include <set>
 
 int main()
 {
-	// 시퀀스 컨테이너 (Sequence Container)
-	// 데이터가 삽입 순서대로 나열되는 형태
-	// vector list deque
+	// 키 자체가 데이터일경우 set
+	// key == value 가 같은 자료구조이다.
+	set<int>s;
 
-	// vector와 마찬가지로 배열 기반으로 동작
-	// 다만 메모리 할당 정책이 다르다
+	// 넣고
+	// 빼고
+	// 찾고
+	// 순회한다.
 
-	// vector
-	// [1 1 1]
+	// 넣고
+	s.insert(10);
+	s.insert(30);
+	s.insert(20);
+	s.insert(50);
+	s.insert(40);
+	s.insert(70);
+	s.insert(100);
+	s.insert(80);
+	s.insert(90);
 
-	// deque
-	// [    3 3]	1동
-	// [3 4 3 3]	2동
-	// [1 1 1 2]	3동
-	// [2      ]	4동
+	// 빼기
+	s.erase(40);
 
-	vector<int> v(3,1);
-	deque<int> dq(3, 1);
+	// 찾기
+	set<int>::iterator findit = s.find(50);
+	if (findit == s.end())
+	{
+		cout << "못찾음" << endl;
+	} 
+	else
+	{
+		cout << "찾음" << endl;
+	}
 
-	v.push_back(2);
-	v.push_back(2);
+	// 순회하기
+	for (set<int>::iterator it = s.begin(); it != s.end(); it++)
+	{
+		cout << (*it) <<endl;
+	}
 
-	dq.push_back(2);
-	dq.push_back(2);
+	cout << "=====================" << endl;
 
-	dq.push_front(3);
-	dq.push_front(3);
+	multimap<int, int> mm;
 
-	// - deque의 동작원리
-	// - 중간 삽입/삭제 = 매우 느리다
-	// -- 배열이기에 중간에 삭제하면 밀린 공간을 전부 정렬해줘야한다.
-	// - 처음/끝 삽입/삭제 = 둘다 빠르다
-	// - 임의접근  = 빠르다. 
+	// 넣기
+	// 같은키로 다른 값들을 넣을 수 있다.
+	mm.insert(make_pair(1, 100));
+	mm.insert(make_pair(1, 200));
+	mm.insert(make_pair(1, 300));
+	mm.insert(make_pair(2, 300));
+	mm.insert(make_pair(2, 400));
+	mm.insert(make_pair(2, 500));
 
-	dq[3] = 10;
+	// mm[1] = 500; 덮어쓰는거와 같기에 막혀있다.
 
-	deque<int>::iterator it;
+	// 빼기
+	// mm.erase(1);	// 1의 키값을 전부 삭제한다, 반환 값은 삭제된 횟수를 리턴해준다.
 
-	// _Size_type _Block = _Mycont->_Getblock(_Myoff);
-	// _Size_type _Off = _Myoff % _Block_size;
-	// return _Mycont->_Map[_Block][_Off];
+	// 찾기
+	/*multimap<int, int>::iterator itFind = mm.find(0);
+	if(itFind != mm.end())
+		mm.erase(itFind);*/
+
+
+	pair<multimap<int, int>::iterator, multimap<int, int>::iterator> itPair;
+	mm.equal_range(1);
+
+	multimap<int, int>::iterator itBegin = mm.lower_bound(1);
+	multimap<int, int>::iterator itEnd = mm.upper_bound(1);
+
+	for (multimap<int, int>::iterator it = itPair.first; it != itPair.second; ++it)
+	{
+		cout << it->first << " " << it->second <<endl;
+	}
+
+	multiset<int> ms;
+
+	// 넣기
+	ms.insert(100);
+	ms.insert(100);
+	ms.insert(100);
+	ms.insert(200);
+	ms.insert(200);
+
+	// 찾기
+	multiset<int>::iterator findit2 = ms.find(100);
+
+	pair<multiset<int>::iterator, multiset<int>::iterator> itPair2;
+	itPair2 = ms.equal_range(100);
+
+	for (multiset<int>::iterator it = itPair2.first; it != itPair2.second; ++it)
+	{
+		cout << *it << endl;
+	}
+
+	multiset<int>::iterator itBegin2 = ms.lower_bound(100);
+	multiset<int>::iterator itEnd2 = ms.upper_bound(100);
+
+	for (multiset<int>::iterator it = itBegin2; it != itEnd2; ++it)
+	{
+		cout << *it << endl;
+	}
+
 
 
 	return 0;
