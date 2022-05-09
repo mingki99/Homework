fe072834728187cc104c679d69e461a305176983
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-05-09 16:24:17 +0900

    알고리즘 find_if, remove_if, for_each, count_if, all_of

--- I Make Projact/RPGProject_CPP/main.cpp
+++ I Make Projact/RPGProject_CPP/main.cpp
@@ -4,12 +4,25 @@ using namespace std;
 #include <vector>
 #include <deque>
 #include <map>
-
-// 오늘의 주제 : set, multimap, multiset
 #include <set>
+
+// 오늘의 주제 : 알고리즘
+#include <algorithm>
 
 int main()
 {
+	// 자료구조 (데이터를 저장하는 구조)
+	// 알고리즘 (데이터를 어떻게 사용할 것인가?)
+
+	// find
+	// find_if
+
+	// count
+	// count_if
+	// all_of
+	// none_of
+
+	
 	srand(static_cast<unsigned int>(time(nullptr)));
 
 	vector<int> v;
@@ -26,7 +39,7 @@ int main()
 		int count = 1;
 
 		bool found = false;
-		for (vector<int>::iterator it = v.begin(); it != v.end();)
+		for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
 		{	
 			if (*it== number)
 			{
@@ -35,18 +48,28 @@ int main()
 				cout << count << "번 째 에 50 등장" << endl;
 				break;
 			}
-			else
-			{
-				++it;
-				++count;
-			}
+			
+			++count;
+			
 
 
 		}
 
+		// 어디부터, 어디까지, 어떤 데이터를 찾아주는 find
+		vector<int>::iterator itfind = find(v.begin(), v.end(), number);
+		if(itfind == v.end())
+		{
+			// 못찾았음
+			cout << "못 찾았음 " << endl;
+		}
+		else
+		{
+			cout << "찾았다" << endl;
+		}
+
 	}
 
-	// 11로 나뉘는 숫자가 백터에 있는지 체크한다.
+	// 11로 나뉘는 숫자가 백터에 있는지 체크한다. (bool, 첫 등장 iterator)
 	{
 		int count = 1;
 		for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
@@ -58,6 +81,27 @@ int main()
 		
 			++count;
 		}
+		
+		struct CanDivideBy11
+		{
+			bool operator()(int a)
+			{
+				return (a%11) == 0;
+			}
+		};
+
+		// 처음과 끝, 그다음 조건이 bool 을 뱉어주는 
+		vector<int>::iterator itFind = std::find_if(v.begin(),v.end(),CanDivideBy11());
+		if (itFind == v.end())
+		{
+			// 못찾았음
+			cout << "못 찾았음 " << endl;
+		}
+		else
+		{
+			cout << "찾았다" << endl;
+		}
+
 	}
 
 	// 홀수의 숫자의 갯수는?
@@ -72,6 +116,24 @@ int main()
 		}
 		cout << " 홀수의 개수 : "  << count << endl;
 
+		struct IsOdd
+		{
+			bool operator()(int a)
+			{
+				return (a%2) == 0;
+			}
+		};
+
+		// 몇번 카운트 되었는지 리턴
+		int n = std::count_if(v.begin(), v.end(),IsOdd());
+
+		// 모든데이터가 조건에 맞냐
+		bool b1 = std::all_of(v.begin(), v.end(), IsOdd());
+		// 모든 데이터가 하나라도 있냐
+		bool b2 = std::any_of(v.begin(), v.end(), IsOdd());
+		// 모든 데이터가 조건에 안맞냐
+		bool b3 = std::none_of(v.begin(), v.end(), IsOdd());
+			
 	}
 
 	cout << "============================" << endl;
@@ -85,6 +147,60 @@ int main()
 			
 			cout << i <<" 번째 백터에 데이터를 3을 곱해준 값 : " << v[i] << endl;
 		}
+
+		struct MultiplyBy3
+		{
+			void operator()(int a)
+			{
+				a *= 3;
+			}
+		};
+
+		// 값을 하나하나 꺼내어 준다.
+		std::for_each(v.begin(), v.end(), MultiplyBy3());
+	}
+
+	// 홀수인 데이터 일괄 삭제
+	{
+		for (vector<int>::iterator it = v.begin(); it != v.end();)
+		{
+			if((*it %2) !=0)
+				it = v.erase(it);
+			else
+				++it;
+		}
+
+
+		v.clear();
+
+		v.push_back(1);
+		v.push_back(4);
+		v.push_back(3);
+		v.push_back(5);
+		v.push_back(8);
+		v.push_back(2);
+		
+
+		// 1 4 3 5 8 2
+		// 4 8 2 5 8 2
+		// 
+		// 4 8 2 5 8 2
+		struct IsOdd
+		{
+			bool operator()(int a)
+			{
+				return (a % 2) != 0;
+			}
+		};
+
+		// 삭제를 해주는겄이 아닌 필요한 데이터를 남겨준다.
+		// 앞에는 필요한데이터로 생성해주고, 버려질 데이터들은 iterator로 반환해준다.
+		vector<int>::iterator it = std::remove_if(v.begin(), v.end(), IsOdd());
+
+		// iterator로 받은 값을 끝날때까지 지워준다.
+		v.erase(it, v.end());
+
+		int a = 3;
 	}
 
 	return 0;
