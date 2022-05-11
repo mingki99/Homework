e230faaa06c673bc97c602aa216154f54e0ef570
mingki99 <90670024+mingki99@users.noreply.github.com>
2022-05-11 20:18:00 +0900

    백준 10757번 문제

--- Practice Note/Project1/main.cpp
+++ Practice Note/Project1/main.cpp
@@ -1,39 +1,76 @@
 #include <iostream>
 
 using namespace std;
-
+#include <string.h>
 
 
 int main()
 {
+	string a;
+	string b;
+	cin >> a >> b;
 
-	int N;
-	cin >> N;	
+	// 두 수의 합이 옮겨질 변수
+	string temp;
 
-	int temp = N / 5;	// Greedy Algorithm
+	// 십의 자리올라가는 변수
+	int tenth = 0;
 
-	int count = temp;
+	// 사이즈 크기 각각 배분
+	int asize = a.size() - 1;
+	int bsize = b.size() - 1;
 
-	while (1)
+	// a,b size 2개다 0보다 작아지면 반복문 중당
+	while (0 <= asize || 0 <= bsize)
 	{
-		if (temp < 0)
+		// 더해준 값 임시로 저장
+		int add = 0;
+
+		// a,b size 십의자리가 같거나, 다를 때 처리해주는 식과 조건문 
+		if (0 <= asize && 0 <= bsize)
 		{
-			cout << "-1" <<endl;
-			break;
+			add = (a[asize] - 48) + (b[bsize] - 48) + tenth;
 		}
-		
-		if ((N - (temp*5))%3 == 0)
+		else if (asize > bsize)
 		{
-			count += (N - (temp*5)) / 3;
-			cout << count << endl;
-			break;
+			add = (a[asize] - 48) + tenth;
+		}
+		else if (asize < bsize)
+		{
+			add = (b[bsize] - 48) + tenth;
+		}
+
+		// 더해준 값이 10보다 크다면 10의 자리 올림을 해야한다.
+		if (add >= 10)
+		{
+			tenth = add / 10;
+			add %= 10;
+
+			temp.push_back(add);
 		}
 		else
 		{
-			temp -= 1;
-			count -= 1;
+			temp.push_back(add);
+			tenth = 0;
 		}
-
+		
+		--asize;
+		--bsize;
 	}
+	
+	// 첫째 자리수 10의 올림
+	if (tenth == 1)
+	{
+		temp.push_back(tenth);
+	}
+
+	// string을 현재 거꾸로 출력
+	for (int i = (temp.size()) - 1; i >= 0; --i)
+	{
+		int ch = temp [i];
+		cout << ch;
+	}
+	
+
 	return 0;
 }
